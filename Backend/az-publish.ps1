c84f1569e5cc49cdcb16d7f2eda9ef90e80fe7c5
Paweł Kołodziej <pawelkolodziej81@gmail.com>
2025-06-20 21:17:29 +0200

    azure publish script

--- Backend/az-publish.ps1
+++ Backend/az-publish.ps1
@@ -1,10 +1,13 @@
 Remove-Item -Path .\publish -Recurse -Force
 Remove-Item -Path .\publish.zip -Force
+
 # This script builds and publishes a .NET application to an Azure Web App
 # Targeting a Linux environment without self-contained deployment
-dotnet publish -c Release -r linux-x64 --self-contained false -o ./publish
+dotnet publish -c Release -o ./publish --self-contained false
+
 # Compress the publish directory into a zip file
 Compress-Archive -Path .\publish\* -DestinationPath .\publish.zip
+
 # Deploy the zip file to Azure Web App
 # Ensure you have the Azure CLI installed and logged in
-az webapp deploy --resource-group ArtBooking --name artbook --src-path .\publish.zip
+az webapp deploy -g ArtBooking -n Artbook-1 --src-path .\publish.zip
